46b8ac63510fb2c761a4dffc05e2655ca92cb1f2
Neil Massey <neil.massey@valtech.com>
2022-09-29 20:12:39 +0100

    EIP1-1939 Refactor to have test notify client bean to load in application context

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/config/IntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/config/IntegrationTest.kt
@@ -2,17 +2,25 @@ package uk.gov.dluhc.notificationsapi.config
 
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import uk.gov.dluhc.notificationsapi.client.GovNotifyApiClient
 import uk.gov.dluhc.notificationsapi.testsupport.WiremockService
+import uk.gov.service.notify.NotificationClient
 
 /**
  * Base class used to bring up the entire Spring ApplicationContext
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = [IntegrationTest.IntegrationTestConfiguration::class],
+)
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient(timeout = "PT5M")
 internal abstract class IntegrationTest {
@@ -29,5 +37,13 @@ internal abstract class IntegrationTest {
     @BeforeEach
     fun resetWireMock() {
         wireMockService.resetAllStubsAndMappings()
+    }
+
+    @TestConfiguration
+    class IntegrationTestConfiguration {
+        @Bean
+        @Primary
+        fun testNotificationClient(@Value("\${api.notify.api-key}") apiKey: String, wiremockService: WiremockService) =
+            NotificationClient(apiKey, wiremockService.wiremockBaseUrl())
     }
 }
