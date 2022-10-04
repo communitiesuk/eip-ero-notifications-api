8edc8cb5810ca9732936ecb8e0d39ae487aec314
Neil Massey <neil.massey@valtech.com>
2022-10-04 17:35:22 +0100

    EIP1-557 Add test for send email

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/client/GovNotifyApiClientTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/client/GovNotifyApiClientTest.kt
@@ -14,8 +14,13 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.given
 import org.mockito.kotlin.verify
 import uk.gov.dluhc.notificationsapi.dto.api.NotifyTemplatePreviewDto
+import uk.gov.dluhc.notificationsapi.model.NotificationType
 import uk.gov.dluhc.notificationsapi.testsupport.model.NotifyGenerateTemplatePreviewSuccessResponse
+import uk.gov.dluhc.notificationsapi.testsupport.model.NotifySendEmailSuccessResponse
+import uk.gov.dluhc.notificationsapi.testsupport.model.Template
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.DataFaker
 import uk.gov.service.notify.NotificationClient
+import uk.gov.service.notify.SendEmailResponse
 import uk.gov.service.notify.TemplatePreview
 import java.time.LocalDate
 import java.util.UUID
@@ -26,7 +31,43 @@ internal class GovNotifyApiClientTest {
     private lateinit var govNotifyApiClient: GovNotifyApiClient
 
     @Mock
+    private lateinit var notificationTemplateMapper: NotificationTemplateMapper
+
+    @Mock
     private lateinit var notificationClient: NotificationClient
+
+    @Nested
+    inner class SendEmail {
+        @ParameterizedTest
+        @ValueSource(strings = ["html body"])
+        @NullSource
+        fun `should generate template preview given existing html`(html: String?) {
+            // Given
+            val notificationType = NotificationType.APPLICATION_RECEIVED
+            val emailAddress = DataFaker.faker.internet().emailAddress()
+            val reference = UUID.randomUUID()
+            val templateId = UUID.randomUUID().toString()
+            given(notificationTemplateMapper.fromNotificationType(any())).willReturn(templateId)
+            val response =
+                NotifySendEmailSuccessResponse(template = Template(id = templateId), reference = reference.toString())
+            val objectMapper = ObjectMapper()
+            val sendEmailResponse = SendEmailResponse(objectMapper.writeValueAsString(response))
+            val personalisation = mapOf(
+                "subject_param" to "test subject",
+                "name_param" to "John",
+                "custom_title" to "Resubmitting photo",
+                "date" to LocalDate.now()
+            )
+            given(notificationClient.sendEmail(any(), any(), any(), any())).willReturn(sendEmailResponse)
+
+            // When
+            govNotifyApiClient.sendEmail(notificationType, emailAddress, personalisation, reference)
+
+            // Then
+            verify(notificationTemplateMapper).fromNotificationType(notificationType)
+            verify(notificationClient).sendEmail(templateId, emailAddress, personalisation, reference.toString())
+        }
+    }
 
     @Nested
     inner class GenerateTemplatePreview {
