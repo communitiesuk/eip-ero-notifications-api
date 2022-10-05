c59cd25599139dfd2fbaf2e5b005bb927c330f6b
Abdullah Sanver <abdullah.sanver@valtech.com>
2022-10-05 17:50:36 +0100

    Implement 400 scenario

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/rest/TemplateController.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/rest/TemplateController.kt
@@ -1,16 +1,27 @@
 package uk.gov.dluhc.notificationsapi.rest
 
+import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.InitBinder
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.dluhc.notificationsapi.config.TemplateTypeEditor
+import uk.gov.dluhc.notificationsapi.models.GenerateTemplatePreviewRequest
 import uk.gov.dluhc.notificationsapi.models.TemplateType
+import javax.validation.Valid
 
 @RestController
 @CrossOrigin
 class TemplateController {
+    @InitBinder
+    fun initBinder(dataBinder: WebDataBinder) {
+        dataBinder.registerCustomEditor(TemplateType::class.java, TemplateTypeEditor())
+    }
+
     @PostMapping("/templates/{templateType}/preview")
-    fun generateTemplatePreview(@PathVariable templateType: TemplateType): String {
+    fun generateTemplatePreview(@PathVariable templateType: TemplateType, @Valid @RequestBody request: GenerateTemplatePreviewRequest): String {
         return ""
     }
 }
