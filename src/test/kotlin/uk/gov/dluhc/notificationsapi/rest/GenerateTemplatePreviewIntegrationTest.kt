82047b5766221f811ebab341857152b8a73e2fc2
Abdullah Sanver <39367710+adacayi@users.noreply.github.com>
2022-10-06 08:39:37 +0100

    EIP1-1934: EIP1-1996: Implement 400 scenario (#15)

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/rest/GenerateTemplatePreviewIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/rest/GenerateTemplatePreviewIntegrationTest.kt
@@ -3,12 +3,16 @@ package uk.gov.dluhc.notificationsapi.rest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.web.util.UriComponentsBuilder
+import reactor.core.publisher.Mono
 import uk.gov.dluhc.notificationsapi.config.IntegrationTest
+import uk.gov.dluhc.notificationsapi.models.GenerateTemplatePreviewRequest
 import uk.gov.dluhc.notificationsapi.models.TemplateType.APPLICATION_MINUS_APPROVED
 import uk.gov.dluhc.notificationsapi.models.TemplateType.APPLICATION_MINUS_RECEIVED
 import uk.gov.dluhc.notificationsapi.testsupport.bearerToken
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.UNAUTHORIZED_BEARER_TOKEN
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.getBearerToken
 
 internal class GenerateTemplatePreviewIntegrationTest : IntegrationTest() {
     @BeforeEach
@@ -36,7 +40,45 @@ internal class GenerateTemplatePreviewIntegrationTest : IntegrationTest() {
             .isForbidden
     }
 
-    private fun buildUri(templateType: String) =
+    @Test
+    fun `should return bad request given invalid template type`() {
+        webTestClient.post()
+            .uri(buildUri("invalid-template"))
+            .withAValidBody()
+            .bearerToken(getBearerToken())
+            .exchange()
+            .expectStatus()
+            .isBadRequest
+    }
+
+    @Test
+    fun `should return bad request given invalid request`() {
+        webTestClient.post()
+            .uri(buildUri())
+            .bearerToken(getBearerToken())
+            .exchange()
+            .expectStatus()
+            .isBadRequest
+    }
+
+    @Test
+    fun `should return ok given valid template type and request`() {
+        webTestClient.post()
+            .uri(buildUri())
+            .bearerToken(getBearerToken())
+            .withAValidBody()
+            .exchange()
+            .expectStatus()
+            .isOk
+    }
+
+    private fun buildUri(templateType: String = "photo-resubmission") =
         UriComponentsBuilder.fromUriString("/templates/{templateType}/preview").buildAndExpand(templateType)
             .toUriString()
+
+    private fun WebTestClient.RequestBodySpec.withAValidBody(): WebTestClient.RequestBodySpec =
+        body(
+            Mono.just(GenerateTemplatePreviewRequest(personalisation = mapOf())),
+            GenerateTemplatePreviewRequest::class.java
+        ) as WebTestClient.RequestBodySpec
 }
