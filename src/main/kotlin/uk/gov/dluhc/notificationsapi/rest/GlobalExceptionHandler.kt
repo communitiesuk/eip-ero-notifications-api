29569c3214883a7465ab3df8d39c95476a4a698d
Abdullah Sanver <abdullah.sanver@valtech.com>
2022-10-06 12:46:57 +0100

    Return error response body for invalid path value

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/rest/GlobalExceptionHandler.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/rest/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package uk.gov.dluhc.notificationsapi.rest
 
+import org.springframework.beans.TypeMismatchException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,6 +11,7 @@ import org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import uk.gov.dluhc.notificationsapi.config.ApiRequestErrorAttributes
+import javax.servlet.RequestDispatcher
 import javax.servlet.RequestDispatcher.ERROR_STATUS_CODE
 
 @ControllerAdvice
@@ -39,5 +41,17 @@ class GlobalExceptionHandler(
         val body = errorAttributes.getErrorResponse(request)
 
         return handleExceptionInternal(e, body, headers, status, request)
+    }
+
+    override fun handleTypeMismatch(
+        ex: TypeMismatchException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        request.setAttribute(ERROR_STATUS_CODE, status.value(), SCOPE_REQUEST)
+        request.setAttribute(RequestDispatcher.ERROR_MESSAGE, ex.cause?.message ?: "", SCOPE_REQUEST)
+        val body = errorAttributes.getErrorResponse(request)
+        return handleExceptionInternal(ex, body, headers, status, request)
     }
 }
