697515c9e1c265c9ae5062041d07f8e27d8e2c51
Abdullah Sanver <abdullah.sanver@valtech.com>
2023-01-30 17:25:20 +0000

    Address PR suggestions

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/dto/ResubmissionTemplatePreviewDto.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/dto/ResubmissionTemplatePreviewDto.kt
@@ -1,11 +1,5 @@
 package uk.gov.dluhc.notificationsapi.dto
 
-abstract class BaseGenerateTemplatePreviewDto(
-    val channel: NotificationChannel,
-    val language: LanguageDto,
-    val notificationType: NotificationType
-)
-
 class GenerateIdDocumentResubmissionTemplatePreviewDto(
     channel: NotificationChannel,
     language: LanguageDto,
--- src/main/kotlin/uk/gov/dluhc/notificationsapi/mapper/ApplicationRejectionReasonMapper.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/mapper/ApplicationRejectionReasonMapper.kt
@@ -1,20 +1,18 @@
 package uk.gov.dluhc.notificationsapi.mapper
 
-import org.mapstruct.Mapper
-import org.mapstruct.ValueMapping
+import org.springframework.context.MessageSource
+import org.springframework.stereotype.Component
 import uk.gov.dluhc.notificationsapi.models.ApplicationRejectionReason
+import java.util.Locale.ENGLISH
 
-@Mapper
-interface ApplicationRejectionReasonMapper {
-    @ValueMapping(source = "INCOMPLETE_MINUS_APPLICATION", target = "Application is incomplete")
-    @ValueMapping(source = "INACCURATE_MINUS_INFORMATION", target = "Application contains inaccurate information")
-    @ValueMapping(source = "PHOTO_MINUS_IS_MINUS_NOT_MINUS_ACCEPTABLE", target = "Photo does not meet criteria")
-    @ValueMapping(
-        source = "NO_MINUS_RESPONSE_MINUS_FROM_MINUS_APPLICANT",
-        target = "Applicant has not responded to requests for information"
-    )
-    @ValueMapping(source = "FRAUDULENT_MINUS_APPLICATION", target = "Suspected fraudulent application")
-    @ValueMapping(source = "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE", target = "Applicant is not registered to vote")
-    @ValueMapping(source = "OTHER", target = "Other")
-    fun toApplicationRejectionReasonMessage(rejectionReason: ApplicationRejectionReason): String
+@Component
+class ApplicationRejectionReasonMapper(private val messageSource: MessageSource) {
+
+    fun toApplicationRejectionReasonMessage(applicationRejectionReason: ApplicationRejectionReason): String {
+        return messageSource.getMessage(
+            "templates.application-rejection.rejection-reasons.${applicationRejectionReason.value}",
+            null,
+            ENGLISH
+        )
+    }
 }
