19811cac1b1cb7d02925006c39d75befbb417994
hannah.mayhew <hannah.mayhew@valtech.com>
2023-02-27 13:43:56 +0000

    EIP1-4159 Added shell for ReceivedMessageListener

---
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/messaging/SendNotifyApplicationReceivedMessageListener.kt
@@ -0,0 +1,32 @@
+package uk.gov.dluhc.notificationsapi.messaging
+
+import io.awspring.cloud.messaging.listener.annotation.SqsListener
+import mu.KotlinLogging
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Component
+import uk.gov.dluhc.notificationsapi.mapper.TemplatePersonalisationDtoMapper
+import uk.gov.dluhc.notificationsapi.messaging.mapper.SendNotifyMessageMapper
+import uk.gov.dluhc.notificationsapi.messaging.mapper.TemplatePersonalisationMessageMapper
+import uk.gov.dluhc.notificationsapi.messaging.models.SendNotifyApplicationReceivedMessage
+import uk.gov.dluhc.notificationsapi.service.SendNotificationService
+import javax.validation.Valid
+
+private val logger = KotlinLogging.logger { }
+
+@Component
+class SendNotifyApplicationReceivedMessageListener(
+    private val sendNotificationService: SendNotificationService,
+    private val sendNotifyMessageMapper: SendNotifyMessageMapper,
+    private val templatePersonalisationMessageMapper: TemplatePersonalisationMessageMapper,
+    private val templatePersonalisationDtoMapper: TemplatePersonalisationDtoMapper,
+) : MessageListener<SendNotifyApplicationReceivedMessage> {
+
+    @SqsListener(value = ["\${sqs.send-uk-gov-notify-application-received-queue-name}"])
+    override fun handleMessage(@Valid @Payload payload: SendNotifyApplicationReceivedMessage) {
+        logger.info {
+            "received 'send UK Gov notify application received message' request for gssCode: ${payload.gssCode} with " +
+                "messageType: ${payload.messageType}, " +
+                "language: ${payload.language}"
+        }
+    }
+}
