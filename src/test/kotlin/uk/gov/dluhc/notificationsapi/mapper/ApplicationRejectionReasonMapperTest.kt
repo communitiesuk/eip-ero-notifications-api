1e73f8b1cbe14b57fbbb42f5f4e883750dd15789
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-03-16 17:12:57 +0000

    EIP1-4706 - Added new application rejection reasons (#92)

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/ApplicationRejectionReasonMapperTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/ApplicationRejectionReasonMapperTest.kt
@@ -2,58 +2,48 @@ package uk.gov.dluhc.notificationsapi.mapper
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.junit.jupiter.MockitoExtension
-import org.mockito.kotlin.any
-import org.mockito.kotlin.anyOrNull
-import org.mockito.kotlin.given
-import org.mockito.kotlin.verify
-import org.springframework.context.MessageSource
-import java.util.Locale.ENGLISH
+import org.springframework.context.support.ResourceBundleMessageSource
 import uk.gov.dluhc.notificationsapi.messaging.models.ApplicationRejectionReason as ApplicationRejectionReasonMessageEnum
 import uk.gov.dluhc.notificationsapi.models.ApplicationRejectionReason as ApplicationRejectionReasonApiEnum
 
-@ExtendWith(MockitoExtension::class)
 class ApplicationRejectionReasonMapperTest {
-    @InjectMocks
-    private lateinit var mapper: ApplicationRejectionReasonMapper
 
-    @Mock
-    private lateinit var messageSource: MessageSource
+    private val messageSource = ResourceBundleMessageSource().apply {
+        setBasenames("messages")
+        setDefaultEncoding("UTF-8")
+        setFallbackToSystemLocale(true)
+    }
+    private val mapper = ApplicationRejectionReasonMapper(messageSource)
 
     @Nested
     inner class ToApplicationRejectionReasonStringFromApiEnum {
         @ParameterizedTest
         @CsvSource(
             value = [
-                "INCOMPLETE_MINUS_APPLICATION, incomplete-application, Application is incomplete",
-                "INACCURATE_MINUS_INFORMATION, inaccurate-information, Application contains inaccurate information",
-                "PHOTO_MINUS_IS_MINUS_NOT_MINUS_ACCEPTABLE, photo-is-not-acceptable, Photo does not meet criteria",
-                "NO_MINUS_RESPONSE_MINUS_FROM_MINUS_APPLICANT, no-response-from-applicant, Applicant has not responded to requests for information",
-                "FRAUDULENT_MINUS_APPLICATION, fraudulent-application, Suspected fraudulent application",
-                "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE, not-registered-to-vote, Applicant is not registered to vote",
-                "OTHER, other, Other"
+                "INCOMPLETE_MINUS_APPLICATION, Your application was incomplete",
+                "INACCURATE_MINUS_INFORMATION, Your application contained inaccurate information",
+                "PHOTO_MINUS_IS_MINUS_NOT_MINUS_ACCEPTABLE, Your photo did not meet the criteria",
+                "NO_MINUS_RESPONSE_MINUS_FROM_MINUS_APPLICANT, You did not respond to our requests for information within the timeframe we gave you",
+                "FRAUDULENT_MINUS_APPLICATION, Your application contained inaccurate information",
+                "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE, We have not been able to confirm that you are registered to vote at the address you provided",
+                "DUPLICATE_MINUS_APPLICATION, We have received one or more similar or duplicate applications for the details provided",
+                "NO_MINUS_FRANCHISE_MINUS_TO_MINUS_VOTE, Our records show you are not eligible to vote in polls in Great Britain requiring photo identification",
+                "OTHER, Other"
             ]
         )
         fun `should map enums to human readable messages`(
             rejectionReason: ApplicationRejectionReasonApiEnum,
-            keySuffix: String,
-            value: String
+            expected: String
         ) {
             // Given
-            val key = "templates.application-rejection.rejection-reasons.$keySuffix"
-            given(messageSource.getMessage(any(), anyOrNull(), any())).willReturn(value)
 
             // When
             val actual = mapper.toApplicationRejectionReasonString(rejectionReason)
 
             // Then
-            assertThat(actual).isEqualTo(value)
-            verify(messageSource).getMessage(key, null, ENGLISH)
+            assertThat(actual).isEqualTo(expected)
         }
     }
 
@@ -62,30 +52,28 @@ class ApplicationRejectionReasonMapperTest {
         @ParameterizedTest
         @CsvSource(
             value = [
-                "INCOMPLETE_MINUS_APPLICATION, incomplete-application, Application is incomplete",
-                "INACCURATE_MINUS_INFORMATION, inaccurate-information, Application contains inaccurate information",
-                "PHOTO_MINUS_IS_MINUS_NOT_MINUS_ACCEPTABLE, photo-is-not-acceptable, Photo does not meet criteria",
-                "NO_MINUS_RESPONSE_MINUS_FROM_MINUS_APPLICANT, no-response-from-applicant, Applicant has not responded to requests for information",
-                "FRAUDULENT_MINUS_APPLICATION, fraudulent-application, Suspected fraudulent application",
-                "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE, not-registered-to-vote, Applicant is not registered to vote",
-                "OTHER, other, Other"
+                "INCOMPLETE_MINUS_APPLICATION, Your application was incomplete",
+                "INACCURATE_MINUS_INFORMATION, Your application contained inaccurate information",
+                "PHOTO_MINUS_IS_MINUS_NOT_MINUS_ACCEPTABLE, Your photo did not meet the criteria",
+                "NO_MINUS_RESPONSE_MINUS_FROM_MINUS_APPLICANT, You did not respond to our requests for information within the timeframe we gave you",
+                "FRAUDULENT_MINUS_APPLICATION, Your application contained inaccurate information",
+                "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE, We have not been able to confirm that you are registered to vote at the address you provided",
+                "DUPLICATE_MINUS_APPLICATION, We have received one or more similar or duplicate applications for the details provided",
+                "NO_MINUS_FRANCHISE_MINUS_TO_MINUS_VOTE, Our records show you are not eligible to vote in polls in Great Britain requiring photo identification",
+                "OTHER, Other"
             ]
         )
         fun `should map enums to human readable messages`(
             rejectionReason: ApplicationRejectionReasonMessageEnum,
-            keySuffix: String,
-            value: String
+            expected: String
         ) {
             // Given
-            val key = "templates.application-rejection.rejection-reasons.$keySuffix"
-            given(messageSource.getMessage(any(), anyOrNull(), any())).willReturn(value)
 
             // When
             val actual = mapper.toApplicationRejectionReasonString(rejectionReason)
 
             // Then
-            assertThat(actual).isEqualTo(value)
-            verify(messageSource).getMessage(key, null, ENGLISH)
+            assertThat(actual).isEqualTo(expected)
         }
     }
 }
