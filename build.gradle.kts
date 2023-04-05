7b95f0202aa4b9b913c0eb9c8801caacba688d11
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-04-05 09:34:30 +0100

    EIP1-5318 - Version bump to spring boot 2.7.10 (#105)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.owasp.dependencycheck.reporting.ReportGenerator.Format.HTML
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "2.7.7"
+    id("org.springframework.boot") version "2.7.10"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.21"
     kotlin("kapt") version "1.7.21"
