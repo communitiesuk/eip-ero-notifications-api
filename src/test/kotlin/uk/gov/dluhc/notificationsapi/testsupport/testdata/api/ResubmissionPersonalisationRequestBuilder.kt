fb34acf47b857557acf3a1e89db453b5882c0536
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-05-23 15:23:45 +0100

    EIP1-4790 - openAPI and SQS spec changes so that IdDocumentResubmission messages include array of RejectedDocument (#130)

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/testsupport/testdata/api/ResubmissionPersonalisationRequestBuilder.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/testsupport/testdata/api/ResubmissionPersonalisationRequestBuilder.kt
@@ -5,16 +5,18 @@ import uk.gov.dluhc.notificationsapi.models.ContactDetails
 import uk.gov.dluhc.notificationsapi.models.IdDocumentPersonalisation
 import uk.gov.dluhc.notificationsapi.models.PhotoPersonalisation
 import uk.gov.dluhc.notificationsapi.models.PhotoRejectionReason
-import uk.gov.dluhc.notificationsapi.testsupport.testdata.DataFaker
+import uk.gov.dluhc.notificationsapi.models.RejectedDocument
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.DataFaker.Companion.faker
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.aValidApplicationReference
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.getAValidPostcode
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.models.buildRejectedDocument
 
 fun buildPhotoResubmissionPersonalisationRequest(
     applicationReference: String = aValidApplicationReference(),
-    firstName: String = DataFaker.faker.name().firstName(),
+    firstName: String = faker.name().firstName(),
     photoRejectionReasons: List<PhotoRejectionReason> = listOf(PhotoRejectionReason.OTHER_MINUS_OBJECTS_MINUS_OR_MINUS_PEOPLE_MINUS_IN_MINUS_PHOTO),
-    photoRejectionNotes: String? = DataFaker.faker.harryPotter().spell(),
-    photoRequestFreeText: String = DataFaker.faker.harryPotter().spell(),
+    photoRejectionNotes: String? = faker.harryPotter().spell(),
+    photoRequestFreeText: String = faker.harryPotter().spell(),
     uploadPhotoLink: String = "http://localhost:8080/eros/photo/398c1be2-7950-48a2-aca8-14cb9276a673",
     eroContactDetails: ContactDetails = buildContactDetailsRequest()
 ): PhotoPersonalisation =
@@ -30,22 +32,24 @@ fun buildPhotoResubmissionPersonalisationRequest(
 
 fun buildIdDocumentResubmissionPersonalisationRequest(
     applicationReference: String = aValidApplicationReference(),
-    firstName: String = DataFaker.faker.name().firstName(),
-    photoRequestFreeText: String = DataFaker.faker.harryPotter().spell(),
-    eroContactDetails: ContactDetails = buildContactDetailsRequest()
+    firstName: String = faker.name().firstName(),
+    photoRequestFreeText: String = faker.harryPotter().spell(),
+    eroContactDetails: ContactDetails = buildContactDetailsRequest(),
+    rejectedDocuments: List<RejectedDocument> = listOf(buildRejectedDocument()),
 ): IdDocumentPersonalisation =
     IdDocumentPersonalisation(
         applicationReference = applicationReference,
         firstName = firstName,
         idDocumentRequestFreeText = photoRequestFreeText,
-        eroContactDetails = eroContactDetails
+        eroContactDetails = eroContactDetails,
+        rejectedDocuments = rejectedDocuments
     )
 
 fun buildContactDetailsRequest(
     localAuthorityName: String = "some-city-council",
-    website: String = DataFaker.faker.company().url(),
-    phone: String = DataFaker.faker.phoneNumber().cellPhone(),
-    email: String = DataFaker.faker.internet().emailAddress(),
+    website: String = faker.company().url(),
+    phone: String = faker.phoneNumber().cellPhone(),
+    email: String = faker.internet().emailAddress(),
     address: Address = buildAddressRequest(),
 ): ContactDetails =
     ContactDetails(
@@ -57,11 +61,11 @@ fun buildContactDetailsRequest(
     )
 
 fun buildAddressRequest(
-    street: String = DataFaker.faker.address().streetName(),
-    property: String? = DataFaker.faker.address().buildingNumber(),
-    locality: String? = DataFaker.faker.address().streetName(),
-    town: String? = DataFaker.faker.address().city(),
-    area: String? = DataFaker.faker.address().state(),
+    street: String = faker.address().streetName(),
+    property: String? = faker.address().buildingNumber(),
+    locality: String? = faker.address().streetName(),
+    town: String? = faker.address().city(),
+    area: String? = faker.address().state(),
     postcode: String = getAValidPostcode(),
 ): Address = Address(
     street = street,
---
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/testsupport/testdata/messaging/models/SendNotifyPhotoResubmissionMessageBuilder.kt
@@ -0,0 +1,59 @@
+package uk.gov.dluhc.notificationsapi.testsupport.testdata.messaging.models
+
+import uk.gov.dluhc.notificationsapi.messaging.models.ContactDetails
+import uk.gov.dluhc.notificationsapi.messaging.models.Language
+import uk.gov.dluhc.notificationsapi.messaging.models.MessageAddress
+import uk.gov.dluhc.notificationsapi.messaging.models.MessageType
+import uk.gov.dluhc.notificationsapi.messaging.models.NotificationChannel
+import uk.gov.dluhc.notificationsapi.messaging.models.PhotoPersonalisation
+import uk.gov.dluhc.notificationsapi.messaging.models.PhotoRejectionReason
+import uk.gov.dluhc.notificationsapi.messaging.models.SendNotifyPhotoResubmissionMessage
+import uk.gov.dluhc.notificationsapi.messaging.models.SourceType
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.DataFaker.Companion.faker
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.aGssCode
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.aRequestor
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.aSourceReference
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.aValidApplicationReference
+
+fun buildSendNotifyPhotoResubmissionMessage(
+    channel: NotificationChannel = NotificationChannel.EMAIL,
+    language: Language = Language.EN,
+    sourceType: SourceType = SourceType.VOTER_MINUS_CARD,
+    sourceReference: String = aSourceReference(),
+    gssCode: String = aGssCode(),
+    requestor: String = aRequestor(),
+    personalisation: PhotoPersonalisation = buildPhotoPersonalisationMessage(),
+    toAddress: MessageAddress = aMessageAddress(),
+): SendNotifyPhotoResubmissionMessage =
+    SendNotifyPhotoResubmissionMessage(
+        channel = channel,
+        language = language,
+        sourceType = sourceType,
+        sourceReference = sourceReference,
+        gssCode = gssCode,
+        requestor = requestor,
+        messageType = MessageType.PHOTO_MINUS_RESUBMISSION,
+        personalisation = personalisation,
+        toAddress = toAddress,
+    )
+
+fun buildPhotoPersonalisationMessage(
+    applicationReference: String = aValidApplicationReference(),
+    firstName: String = faker.name().firstName(),
+    photoRejectionReasons: List<PhotoRejectionReason> = listOf(PhotoRejectionReason.OTHER),
+    photoRejectionNotes: String? = faker.harryPotter().spell(),
+    photoRequestFreeText: String = faker.harryPotter().spell(),
+    uploadPhotoLink: String = "http://localhost:8080/eros/photo/398c1be2-7950-48a2-aca8-14cb9276a673",
+    eroContactDetails: ContactDetails = buildContactDetailsMessage()
+): PhotoPersonalisation =
+    PhotoPersonalisation(
+        applicationReference = applicationReference,
+        firstName = firstName,
+        photoRejectionReasons = photoRejectionReasons,
+        photoRejectionNotes = photoRejectionNotes,
+        photoRequestFreeText = photoRequestFreeText,
+        uploadPhotoLink = uploadPhotoLink,
+        eroContactDetails = eroContactDetails
+    )
+
+fun aSendNotifyPhotoResubmissionMessage() = buildSendNotifyPhotoResubmissionMessage()
