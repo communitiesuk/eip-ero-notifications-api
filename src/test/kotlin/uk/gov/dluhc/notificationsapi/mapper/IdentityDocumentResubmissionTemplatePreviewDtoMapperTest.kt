d4eeb5ab73390865f4c208ae4845454fff9238ff
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-05-24 14:44:59 +0100

    EIP1-4790 - Code to select the correct template ID for document resubmission comms, based on whether there are rejected documents with reasons (#136)

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/IdentityDocumentResubmissionTemplatePreviewDtoMapperTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/IdentityDocumentResubmissionTemplatePreviewDtoMapperTest.kt
@@ -10,12 +10,17 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.given
 import org.mockito.kotlin.verify
 import uk.gov.dluhc.notificationsapi.dto.LanguageDto
+import uk.gov.dluhc.notificationsapi.dto.NotificationType.ID_DOCUMENT_RESUBMISSION
+import uk.gov.dluhc.notificationsapi.dto.NotificationType.ID_DOCUMENT_RESUBMISSION_WITH_REASONS
+import uk.gov.dluhc.notificationsapi.models.DocumentRejectionReason.DOCUMENT_MINUS_TOO_MINUS_OLD
 import uk.gov.dluhc.notificationsapi.models.Language
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.api.buildGenerateIdDocumentResubmissionTemplatePreviewRequest
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.api.buildIdDocumentResubmissionPersonalisationRequest
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.dto.buildAddressDto
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.dto.buildContactDetailsDto
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.dto.buildGenerateIdDocumentResubmissionTemplatePreviewDto
 import uk.gov.dluhc.notificationsapi.testsupport.testdata.dto.buildIdDocumentPersonalisationDto
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.models.buildRejectedDocument
 import uk.gov.dluhc.notificationsapi.dto.NotificationChannel as NotificationChannelDto
 import uk.gov.dluhc.notificationsapi.dto.SourceType as SourceTypeDto
 import uk.gov.dluhc.notificationsapi.models.NotificationChannel as NotificationChannelApi
@@ -37,12 +42,15 @@ class IdentityDocumentResubmissionTemplatePreviewDtoMapperTest {
     private lateinit var sourceTypeMapper: SourceTypeMapper
 
     @Test
-    fun `should map ID document template request to dto`() {
+    fun `should map ID document template request to dto given no rejected documents`() {
         // Given
         val request = buildGenerateIdDocumentResubmissionTemplatePreviewRequest(
             channel = NotificationChannelApi.LETTER,
             language = Language.EN,
-            sourceType = SourceTypeModel.VOTER_MINUS_CARD
+            sourceType = SourceTypeModel.VOTER_MINUS_CARD,
+            personalisation = buildIdDocumentResubmissionPersonalisationRequest(
+                rejectedDocuments = emptyList()
+            )
         )
 
         given(languageMapper.fromApiToDto(any())).willReturn(LanguageDto.ENGLISH)
@@ -53,6 +61,69 @@ class IdentityDocumentResubmissionTemplatePreviewDtoMapperTest {
             sourceType = SourceTypeDto.VOTER_CARD,
             channel = NotificationChannelDto.LETTER,
             language = LanguageDto.ENGLISH,
+            notificationType = ID_DOCUMENT_RESUBMISSION,
+            personalisation = with(request.personalisation) {
+                buildIdDocumentPersonalisationDto(
+                    applicationReference = applicationReference,
+                    firstName = firstName,
+                    idDocumentRequestFreeText = idDocumentRequestFreeText,
+                    eroContactDetails = with(eroContactDetails) {
+                        buildContactDetailsDto(
+                            localAuthorityName = localAuthorityName,
+                            website = website,
+                            phone = phone,
+                            email = email,
+                            address = with(address) {
+                                buildAddressDto(
+                                    street = street,
+                                    property = property,
+                                    locality = locality,
+                                    town = town,
+                                    area = area,
+                                    postcode = postcode
+                                )
+                            }
+                        )
+                    }
+                )
+            }
+        )
+
+        // When
+        val actual = mapper.toIdDocumentResubmissionTemplatePreviewDto(request)
+
+        // Then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected)
+        verify(languageMapper).fromApiToDto(Language.EN)
+        verify(channelMapper).fromApiToDto(NotificationChannelApi.LETTER)
+        verify(sourceTypeMapper).fromApiToDto(SourceTypeModel.VOTER_MINUS_CARD)
+    }
+
+    @Test
+    fun `should map ID document template request to dto given rejected documents with rejection reasons`() {
+        // Given
+        val request = buildGenerateIdDocumentResubmissionTemplatePreviewRequest(
+            channel = NotificationChannelApi.LETTER,
+            language = Language.EN,
+            sourceType = SourceTypeModel.VOTER_MINUS_CARD,
+            personalisation = buildIdDocumentResubmissionPersonalisationRequest(
+                rejectedDocuments = listOf(
+                    buildRejectedDocument(
+                        rejectionReasons = listOf(DOCUMENT_MINUS_TOO_MINUS_OLD)
+                    )
+                )
+            )
+        )
+
+        given(languageMapper.fromApiToDto(any())).willReturn(LanguageDto.ENGLISH)
+        given(channelMapper.fromApiToDto(any())).willReturn(NotificationChannelDto.LETTER)
+        given(sourceTypeMapper.fromApiToDto(any())).willReturn(SourceTypeDto.VOTER_CARD)
+
+        val expected = buildGenerateIdDocumentResubmissionTemplatePreviewDto(
+            sourceType = SourceTypeDto.VOTER_CARD,
+            channel = NotificationChannelDto.LETTER,
+            language = LanguageDto.ENGLISH,
+            notificationType = ID_DOCUMENT_RESUBMISSION_WITH_REASONS,
             personalisation = with(request.personalisation) {
                 buildIdDocumentPersonalisationDto(
                     applicationReference = applicationReference,
