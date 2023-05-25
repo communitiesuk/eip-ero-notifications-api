8294fbda680da0636537e9d6843c43b6dc9faf88
Nathan Russell <nathan.russell@valtech.com>
2023-05-25 08:47:23 +0100

    EIP1-4790 - Introduce IdentityDocumentResubmissionDocumentRejectionTextMapper

---
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/testsupport/testdata/models/NotificationsIdDocumentPersonalisationBuilder.kt
@@ -0,0 +1,21 @@
+package uk.gov.dluhc.notificationsapi.testsupport.testdata.models
+
+import uk.gov.dluhc.notificationsapi.models.ContactDetails
+import uk.gov.dluhc.notificationsapi.models.IdDocumentPersonalisation
+import uk.gov.dluhc.notificationsapi.models.RejectedDocument
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.DataFaker.Companion.faker
+import uk.gov.dluhc.notificationsapi.testsupport.testdata.aValidApplicationReference
+
+fun buildIdDocumentPersonalisation(
+    applicationReference: String = aValidApplicationReference(),
+    firstName: String = faker.name().firstName(),
+    eroContactDetails: ContactDetails = buildEroContactDetails(),
+    idDocumentRequestFreeText: String = faker.harryPotter().spell(),
+    rejectedDocuments: List<RejectedDocument>? = listOf(buildRejectedDocument())
+) = IdDocumentPersonalisation(
+    applicationReference = applicationReference,
+    firstName = firstName,
+    eroContactDetails = eroContactDetails,
+    idDocumentRequestFreeText = idDocumentRequestFreeText,
+    rejectedDocuments = rejectedDocuments
+)
