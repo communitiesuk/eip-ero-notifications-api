bf7944cff9e00ba25f2da685723fc01b5463e0eb
inderjitvirdi <inderjit.virdi@valtech.com>
2023-06-05 15:45:47 +0100

    EIP1-6412 Add message bundle property for document rejection reason of other (#146)

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/RejectedDocumentReasonMapperTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/RejectedDocumentReasonMapperTest.kt
@@ -29,7 +29,8 @@ class RejectedDocumentReasonMapperTest {
                 "DUPLICATE_MINUS_DOCUMENT, This was a duplicate of another document that you have provided",
                 "INVALID_MINUS_DOCUMENT_MINUS_COUNTRY, We are not able to accept documents from this country for the purposes of checking your identity",
                 "APPLICANT_MINUS_DETAILS_MINUS_NOT_MINUS_CLEAR, The document needs to clearly show your name",
-                "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Information provided on the document does not match information from your application"
+                "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Information provided on the document does not match information from your application",
+                "OTHER, Other"
             ]
         )
         fun `should map enums to human readable messages in English`(
@@ -44,31 +45,32 @@ class RejectedDocumentReasonMapperTest {
             // Then
             assertThat(actual).isEqualTo(expected)
         }
-    }
 
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "DOCUMENT_MINUS_TOO_MINUS_OLD, Mae'r ddogfen yn rhy hen",
-            "UNREADABLE_MINUS_DOCUMENT, Nid oeddem yn gallu darllen y ddogfen a ddarparwyd oherwydd nad oedd yn glir neu oherwydd nad oedd yn dangos y wybodaeth yr oedd ei hangen arnom",
-            "INVALID_MINUS_DOCUMENT_MINUS_TYPE, Nid yw'r ddogfen a ddarparwyd o fath y gallwn ei derbyn at ddibenion gwirio pwy ydych",
-            "DUPLICATE_MINUS_DOCUMENT, Roedd hwn yn gopi dyblyg o ddogfen arall a ddarparwyd gennych",
-            "INVALID_MINUS_DOCUMENT_MINUS_COUNTRY, Ni allwn dderbyn dogfennau o'r wlad hon at ddibenion gwirio pwy ydych",
-            "APPLICANT_MINUS_DETAILS_MINUS_NOT_MINUS_CLEAR, Mae angen i'r ddogfen ddangos eich enw'n glir",
-            "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Nid yw'r wybodaeth a ddarperir ar y ddogfen yn cyfateb i'r wybodaeth o'ch cais"
-        ]
-    )
-    fun `should map enums to human readable messages in Welsh`(
-        rejectionReason: DocumentRejectionReasonApi,
-        expected: String
-    ) {
-        // Given
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "DOCUMENT_MINUS_TOO_MINUS_OLD, Mae'r ddogfen yn rhy hen",
+                "UNREADABLE_MINUS_DOCUMENT, Nid oeddem yn gallu darllen y ddogfen a ddarparwyd oherwydd nad oedd yn glir neu oherwydd nad oedd yn dangos y wybodaeth yr oedd ei hangen arnom",
+                "INVALID_MINUS_DOCUMENT_MINUS_TYPE, Nid yw'r ddogfen a ddarparwyd o fath y gallwn ei derbyn at ddibenion gwirio pwy ydych",
+                "DUPLICATE_MINUS_DOCUMENT, Roedd hwn yn gopi dyblyg o ddogfen arall a ddarparwyd gennych",
+                "INVALID_MINUS_DOCUMENT_MINUS_COUNTRY, Ni allwn dderbyn dogfennau o'r wlad hon at ddibenion gwirio pwy ydych",
+                "APPLICANT_MINUS_DETAILS_MINUS_NOT_MINUS_CLEAR, Mae angen i'r ddogfen ddangos eich enw'n glir",
+                "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Nid yw'r wybodaeth a ddarperir ar y ddogfen yn cyfateb i'r wybodaeth o'ch cais",
+                "OTHER, Eraill"
+            ]
+        )
+        fun `should map enums to human readable messages in Welsh`(
+            rejectionReason: DocumentRejectionReasonApi,
+            expected: String
+        ) {
+            // Given
 
-        // When
-        val actual = mapper.toDocumentRejectionReasonString(rejectionReason, LanguageDto.WELSH)
+            // When
+            val actual = mapper.toDocumentRejectionReasonString(rejectionReason, LanguageDto.WELSH)
 
-        // Then
-        assertThat(actual).isEqualTo(expected)
+            // Then
+            assertThat(actual).isEqualTo(expected)
+        }
     }
 
     @Nested
@@ -82,7 +84,8 @@ class RejectedDocumentReasonMapperTest {
                 "DUPLICATE_MINUS_DOCUMENT, This was a duplicate of another document that you have provided",
                 "INVALID_MINUS_DOCUMENT_MINUS_COUNTRY, We are not able to accept documents from this country for the purposes of checking your identity",
                 "APPLICANT_MINUS_DETAILS_MINUS_NOT_MINUS_CLEAR, The document needs to clearly show your name",
-                "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Information provided on the document does not match information from your application"
+                "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Information provided on the document does not match information from your application",
+                "OTHER, Other"
             ]
         )
         fun `should map enums to human readable messages in English`(
@@ -97,30 +100,31 @@ class RejectedDocumentReasonMapperTest {
             // Then
             assertThat(actual).isEqualTo(expected)
         }
-    }
 
-    @ParameterizedTest
-    @CsvSource(
-        value = [
-            "DOCUMENT_MINUS_TOO_MINUS_OLD, Mae'r ddogfen yn rhy hen",
-            "UNREADABLE_MINUS_DOCUMENT, Nid oeddem yn gallu darllen y ddogfen a ddarparwyd oherwydd nad oedd yn glir neu oherwydd nad oedd yn dangos y wybodaeth yr oedd ei hangen arnom",
-            "INVALID_MINUS_DOCUMENT_MINUS_TYPE, Nid yw'r ddogfen a ddarparwyd o fath y gallwn ei derbyn at ddibenion gwirio pwy ydych",
-            "DUPLICATE_MINUS_DOCUMENT, Roedd hwn yn gopi dyblyg o ddogfen arall a ddarparwyd gennych",
-            "INVALID_MINUS_DOCUMENT_MINUS_COUNTRY, Ni allwn dderbyn dogfennau o'r wlad hon at ddibenion gwirio pwy ydych",
-            "APPLICANT_MINUS_DETAILS_MINUS_NOT_MINUS_CLEAR, Mae angen i'r ddogfen ddangos eich enw'n glir",
-            "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Nid yw'r wybodaeth a ddarperir ar y ddogfen yn cyfateb i'r wybodaeth o'ch cais"
-        ]
-    )
-    fun `should map enums to human readable messages in Welsh`(
-        rejectionReason: DocumentRejectionReasonMessaging,
-        expected: String
-    ) {
-        // Given
+        @ParameterizedTest
+        @CsvSource(
+            value = [
+                "DOCUMENT_MINUS_TOO_MINUS_OLD, Mae'r ddogfen yn rhy hen",
+                "UNREADABLE_MINUS_DOCUMENT, Nid oeddem yn gallu darllen y ddogfen a ddarparwyd oherwydd nad oedd yn glir neu oherwydd nad oedd yn dangos y wybodaeth yr oedd ei hangen arnom",
+                "INVALID_MINUS_DOCUMENT_MINUS_TYPE, Nid yw'r ddogfen a ddarparwyd o fath y gallwn ei derbyn at ddibenion gwirio pwy ydych",
+                "DUPLICATE_MINUS_DOCUMENT, Roedd hwn yn gopi dyblyg o ddogfen arall a ddarparwyd gennych",
+                "INVALID_MINUS_DOCUMENT_MINUS_COUNTRY, Ni allwn dderbyn dogfennau o'r wlad hon at ddibenion gwirio pwy ydych",
+                "APPLICANT_MINUS_DETAILS_MINUS_NOT_MINUS_CLEAR, Mae angen i'r ddogfen ddangos eich enw'n glir",
+                "DETAILS_MINUS_ON_MINUS_DOCUMENT_MINUS_DO_MINUS_NOT_MINUS_MATCH, Nid yw'r wybodaeth a ddarperir ar y ddogfen yn cyfateb i'r wybodaeth o'ch cais",
+                "OTHER, Eraill"
+            ]
+        )
+        fun `should map enums to human readable messages in Welsh`(
+            rejectionReason: DocumentRejectionReasonMessaging,
+            expected: String
+        ) {
+            // Given
 
-        // When
-        val actual = mapper.toDocumentRejectionReasonString(rejectionReason, LanguageDto.WELSH)
+            // When
+            val actual = mapper.toDocumentRejectionReasonString(rejectionReason, LanguageDto.WELSH)
 
-        // Then
-        assertThat(actual).isEqualTo(expected)
+            // Then
+            assertThat(actual).isEqualTo(expected)
+        }
     }
 }
