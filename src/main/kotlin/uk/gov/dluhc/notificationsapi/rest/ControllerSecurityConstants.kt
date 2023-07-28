e23650836bdcc1749ba4c27e66255d17b80ce5a8
inderjitvirdi <inderjit.virdi@valtech.com>
2023-07-28 12:39:22 +0100

    EIP1-6789 Restrict access to comms history to users with matching ero admin authority for application type requested (#160)

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/rest/ControllerSecurityConstants.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/rest/ControllerSecurityConstants.kt
@@ -1,5 +1,10 @@
 package uk.gov.dluhc.notificationsapi.rest
 
+import org.springframework.stereotype.Component
+import uk.gov.dluhc.notificationsapi.models.SourceType
+import uk.gov.dluhc.notificationsapi.models.SourceType.OVERSEAS
+import uk.gov.dluhc.notificationsapi.models.SourceType.VOTER_MINUS_CARD
+
 const val HAS_ERO_VC_ANONYMOUS_ADMIN_AUTHORITY = """
         hasAnyAuthority("ero-vc-anonymous-admin-".concat(#eroId))
     """
@@ -12,10 +17,17 @@ const val HAS_ERO_VC_ADMIN_OR_VC_VIEWER_AUTHORITY = """
                         )
         """
 
-const val HAS_ERO_VC_OR_OAVA_ADMIN_AUTHORITY = """
-        hasAnyAuthority("ero-vc-admin-".concat(#eroId),
-                        "ero-postal-admin-".concat(#eroId),
-                        "ero-proxy-admin-".concat(#eroId),
-                        "ero-oe-admin-".concat(#eroId)
-                        )
+const val HAS_APPLICATION_SPECIFIC_ERO_ADMIN_AUTHORITY = """
+        hasAnyAuthority(@authorityHelper.resolveRequiredAuthority(#eroId, #sourceType))
     """
+
+@Component
+class AuthorityHelper {
+
+    fun resolveRequiredAuthority(eroId: String, sourceType: SourceType): String =
+        when (sourceType) {
+            VOTER_MINUS_CARD -> "ero-vc-admin-$eroId"
+            OVERSEAS -> "ero-oe-admin-$eroId"
+            else -> "ero-${sourceType.value}-admin-$eroId"
+        }
+}
