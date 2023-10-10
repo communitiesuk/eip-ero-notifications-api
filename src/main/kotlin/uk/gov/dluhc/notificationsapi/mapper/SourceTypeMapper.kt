ea9354d4ab707d07e260c7f7aae92500476bfc36
Ralf Leggett <ralf.leggett@softwire.com>
2023-10-10 17:11:33 +0100

    EIP1-7146: Work around issue with SourceTypeMapper and testing

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/mapper/SourceTypeMapper.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/mapper/SourceTypeMapper.kt
@@ -1,32 +1,46 @@
 package uk.gov.dluhc.notificationsapi.mapper
 
-import org.mapstruct.InheritInverseConfiguration
-import org.mapstruct.Mapper
-import org.mapstruct.ValueMapping
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
+import org.springframework.stereotype.Component
 import uk.gov.dluhc.notificationsapi.dto.LanguageDto
+import uk.gov.dluhc.notificationsapi.messaging.models.SourceType
 import uk.gov.dluhc.notificationsapi.database.entity.SourceType as SourceTypeEntityEnum
 import uk.gov.dluhc.notificationsapi.dto.SourceType as SourceTypeDtoEnum
 import uk.gov.dluhc.notificationsapi.messaging.models.SourceType as SourceTypeMessageEnum
 import uk.gov.dluhc.notificationsapi.models.SourceType as SourceTypeApiEnum
 
-@Mapper
-abstract class SourceTypeMapper {
+@Component
+class SourceTypeMapper(private val messageSource: MessageSource) {
 
-    @Autowired
-    private lateinit var messageSource: MessageSource
+    fun fromMessageToDto(sourceType: SourceTypeMessageEnum) = when (sourceType) {
+        SourceType.VOTER_MINUS_CARD -> SourceTypeDtoEnum.VOTER_CARD
+        SourceType.POSTAL -> SourceTypeDtoEnum.POSTAL
+        SourceType.PROXY -> SourceTypeDtoEnum.PROXY
+        SourceType.OVERSEAS -> SourceTypeDtoEnum.OVERSEAS
+    }
 
-    @ValueMapping(source = "VOTER_MINUS_CARD", target = "VOTER_CARD")
-    abstract fun fromMessageToDto(sourceType: SourceTypeMessageEnum): SourceTypeDtoEnum
+    fun fromDtoToEntity(sourceType: SourceTypeDtoEnum) = when (sourceType) {
+        SourceTypeDtoEnum.VOTER_CARD -> SourceTypeEntityEnum.VOTER_CARD
+        SourceTypeDtoEnum.POSTAL -> SourceTypeEntityEnum.POSTAL
+        SourceTypeDtoEnum.ANONYMOUS_ELECTOR_DOCUMENT -> SourceTypeEntityEnum.ANONYMOUS_ELECTOR_DOCUMENT
+        SourceTypeDtoEnum.PROXY -> SourceTypeEntityEnum.PROXY
+        SourceTypeDtoEnum.OVERSEAS -> SourceTypeEntityEnum.OVERSEAS
+    }
 
-    abstract fun fromDtoToEntity(sourceType: SourceTypeDtoEnum): SourceTypeEntityEnum
+    fun fromEntityToDto(sourceType: SourceTypeEntityEnum) = when (sourceType) {
+        SourceTypeEntityEnum.VOTER_CARD -> SourceTypeDtoEnum.VOTER_CARD
+        SourceTypeEntityEnum.POSTAL -> SourceTypeDtoEnum.POSTAL
+        SourceTypeEntityEnum.ANONYMOUS_ELECTOR_DOCUMENT -> SourceTypeDtoEnum.ANONYMOUS_ELECTOR_DOCUMENT
+        SourceTypeEntityEnum.PROXY -> SourceTypeDtoEnum.PROXY
+        SourceTypeEntityEnum.OVERSEAS -> SourceTypeDtoEnum.OVERSEAS
+    }
 
-    @InheritInverseConfiguration
-    abstract fun fromEntityToDto(sourceType: SourceTypeEntityEnum): SourceTypeDtoEnum
-
-    @ValueMapping(source = "VOTER_MINUS_CARD", target = "VOTER_CARD")
-    abstract fun fromApiToDto(sourceType: SourceTypeApiEnum): SourceTypeDtoEnum
+    fun fromApiToDto(sourceType: SourceTypeApiEnum) = when (sourceType) {
+        SourceTypeApiEnum.VOTER_MINUS_CARD -> SourceTypeDtoEnum.VOTER_CARD
+        SourceTypeApiEnum.POSTAL -> SourceTypeDtoEnum.POSTAL
+        SourceTypeApiEnum.PROXY -> SourceTypeDtoEnum.PROXY
+        SourceTypeApiEnum.OVERSEAS -> SourceTypeDtoEnum.OVERSEAS
+    }
 
     fun toSourceTypeString(
         sourceType: SourceTypeApiEnum,
