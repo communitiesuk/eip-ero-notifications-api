14df899190907df8dc7a770d6498961a1250fe1c
chrsop <Christian.Sophocleous@softwire.com>
2024-02-26 11:30:11 +0000

    EIP1-8864: Markups

---
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/mapper/OverseasDocumentTypeMapperTest.kt
@@ -0,0 +1,56 @@
+package uk.gov.dluhc.notificationsapi.mapper
+
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import uk.gov.dluhc.notificationsapi.dto.OverseasDocumentTypeDto
+import uk.gov.dluhc.notificationsapi.models.OverseasDocumentType
+import org.assertj.core.api.Assertions.assertThat
+import uk.gov.dluhc.notificationsapi.dto.NotificationType
+import kotlin.math.exp
+
+class OverseasDocumentTypeMapperTest {
+
+    private val mapper = OverseasDocumentTypeMapperImpl()
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "PARENT_MINUS_GUARDIAN, PARENT_GUARDIAN",
+            "QUALIFYING_MINUS_ADDRESS, QUALIFYING_ADDRESS",
+            "IDENTITY, IDENTITY"
+        ]
+    )
+    fun `should map from overseas document type API to overseas document type DTO`(
+        overseasDocumentTypeApi: OverseasDocumentType,
+        expected: OverseasDocumentTypeDto
+    ) {
+        //Given
+
+        //When
+        val actual = mapper.fromApiToDto(overseasDocumentTypeApi)
+
+        //Then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "IDENTITY, REJECTED_DOCUMENT",
+            "PARENT_GUARDIAN, REJECTED_PARENT_GUARDIAN",
+            "QUALIFYING_ADDRESS, REJECTED_QUALIFYING_ADDRESS"
+        ]
+    )
+    fun `should map from overseas document type DTO to notification type`(
+        overseasDocumentTypeDto: OverseasDocumentTypeDto,
+        expected: NotificationType
+    ) {
+        //Given
+
+        //When
+        val actual = mapper.fromOverseasDocumentTypeDtoToNotificationTypeDto(overseasDocumentTypeDto)
+
+        //Then
+        assertThat(actual).isEqualTo(expected)
+    }
+}
