ab505788af3059d802dfccc6c73eb826d1127f89
ChristianSoftwire <145482922+ChristianSoftwire@users.noreply.github.com>
2024-03-15 15:47:29 +0000

    EIP1-9080: EROP OE notification API add sending rejected doc comms (#181)

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/messaging/SendNotifyRejectedDocumentMessageListenerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/messaging/SendNotifyRejectedDocumentMessageListenerIntegrationTest.kt
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource
 import uk.gov.dluhc.notificationsapi.config.IntegrationTest
 import uk.gov.dluhc.notificationsapi.database.entity.Channel
 import uk.gov.dluhc.notificationsapi.database.entity.NotificationType
+import uk.gov.dluhc.notificationsapi.messaging.models.DocumentCategory
 import uk.gov.dluhc.notificationsapi.messaging.models.Language
 import uk.gov.dluhc.notificationsapi.messaging.models.NotificationChannel
 import uk.gov.dluhc.notificationsapi.messaging.models.SourceType
@@ -24,16 +25,30 @@ private val logger = KotlinLogging.logger {}
 
 internal class SendNotifyRejectedDocumentMessageListenerIntegrationTest : IntegrationTest() {
 
+    companion object {
+        private const val SOURCE_TYPE_POSTAL = "POSTAL"
+        private const val SOURCE_TYPE_PROXY = "PROXY"
+        private const val SOURCE_TYPE_OVERSEAS = "OVERSEAS"
+        private const val NOTIFICATION_TYPE_IDENTITY = "REJECTED_DOCUMENT"
+        private const val NOTIFICATION_TYPE_PREVIOUS_ADDRESS = "REJECTED_PREVIOUS_ADDRESS"
+        private const val NOTIFICATION_TYPE_PARENT_GUARDIAN = "REJECTED_PARENT_GUARDIAN"
+    }
+
     @ParameterizedTest
     @CsvSource(
         value = [
-            "POSTAL, POSTAL",
-            "PROXY, PROXY",
-        ]
+            "$SOURCE_TYPE_POSTAL, $SOURCE_TYPE_POSTAL, IDENTITY, $NOTIFICATION_TYPE_IDENTITY",
+            "$SOURCE_TYPE_PROXY, $SOURCE_TYPE_PROXY, IDENTITY, $NOTIFICATION_TYPE_IDENTITY",
+            "$SOURCE_TYPE_OVERSEAS, $SOURCE_TYPE_OVERSEAS, IDENTITY, $NOTIFICATION_TYPE_IDENTITY",
+            "$SOURCE_TYPE_OVERSEAS, $SOURCE_TYPE_OVERSEAS, PREVIOUS_MINUS_ADDRESS, $NOTIFICATION_TYPE_PREVIOUS_ADDRESS",
+            "$SOURCE_TYPE_OVERSEAS, $SOURCE_TYPE_OVERSEAS, PARENT_MINUS_GUARDIAN, $NOTIFICATION_TYPE_PARENT_GUARDIAN",
+        ],
     )
     fun `should process Rejected Document message to send Email and save notification`(
         sourceType: SourceType,
-        expectedSourceType: SourceTypeEntity
+        sourceTypeEntity: SourceTypeEntity,
+        documentCategory: DocumentCategory,
+        notificationType: NotificationType
     ) {
         // Given
         val gssCode = aGssCode()
@@ -43,7 +58,8 @@ internal class SendNotifyRejectedDocumentMessageListenerIntegrationTest : Integr
             language = Language.EN,
             gssCode = gssCode,
             sourceType = sourceType,
-            sourceReference = sourceReference
+            sourceReference = sourceReference,
+            documentCategory = documentCategory
         )
         wireMockService.stubNotifySendEmailResponse(NotifySendEmailSuccessResponse())
 
@@ -54,10 +70,14 @@ internal class SendNotifyRejectedDocumentMessageListenerIntegrationTest : Integr
         val stopWatch = StopWatch.createStarted()
         await.atMost(3, TimeUnit.SECONDS).untilAsserted {
             wireMockService.verifyNotifySendEmailCalled()
-            val actualEntity = notificationRepository.getBySourceReferenceAndGssCode(sourceReference, expectedSourceType, listOf(gssCode))
+            val actualEntity = notificationRepository.getBySourceReferenceAndGssCode(
+                sourceReference,
+                sourceTypeEntity,
+                listOf(gssCode)
+            )
             assertThat(actualEntity).hasSize(1).element(0)
                 .extracting("sourceType", "type", "channel")
-                .containsExactlyInAnyOrder(expectedSourceType, NotificationType.REJECTED_DOCUMENT, Channel.EMAIL)
+                .containsExactlyInAnyOrder(sourceTypeEntity, notificationType, Channel.EMAIL)
             stopWatch.stop()
             logger.info("completed assertions in $stopWatch for language EN")
         }
@@ -66,13 +86,18 @@ internal class SendNotifyRejectedDocumentMessageListenerIntegrationTest : Integr
     @ParameterizedTest
     @CsvSource(
         value = [
-            "POSTAL, POSTAL",
-            "PROXY, PROXY"
-        ]
+            "$SOURCE_TYPE_POSTAL, $SOURCE_TYPE_POSTAL, IDENTITY, $NOTIFICATION_TYPE_IDENTITY",
+            "$SOURCE_TYPE_PROXY, $SOURCE_TYPE_PROXY, IDENTITY, $NOTIFICATION_TYPE_IDENTITY",
+            "$SOURCE_TYPE_OVERSEAS, $SOURCE_TYPE_OVERSEAS, IDENTITY, $NOTIFICATION_TYPE_IDENTITY",
+            "$SOURCE_TYPE_OVERSEAS, $SOURCE_TYPE_OVERSEAS, PREVIOUS_MINUS_ADDRESS, $NOTIFICATION_TYPE_PREVIOUS_ADDRESS",
+            "$SOURCE_TYPE_OVERSEAS, $SOURCE_TYPE_OVERSEAS, PARENT_MINUS_GUARDIAN, $NOTIFICATION_TYPE_PARENT_GUARDIAN",
+        ],
     )
     fun `should process Rejected Document message to send Letter and save notification`(
         sourceType: SourceType,
-        expectedSourceType: SourceTypeEntity
+        sourceTypeEntity: SourceTypeEntity,
+        documentCategory: DocumentCategory,
+        notificationType: NotificationType
     ) {
         // Given
         val gssCode = aGssCode()
@@ -82,7 +107,8 @@ internal class SendNotifyRejectedDocumentMessageListenerIntegrationTest : Integr
             language = Language.EN,
             gssCode = gssCode,
             sourceType = sourceType,
-            sourceReference = sourceReference
+            sourceReference = sourceReference,
+            documentCategory = documentCategory
         )
         wireMockService.stubNotifySendLetterResponse(NotifySendLetterSuccessResponse())
 
@@ -93,10 +119,14 @@ internal class SendNotifyRejectedDocumentMessageListenerIntegrationTest : Integr
         val stopWatch = StopWatch.createStarted()
         await.atMost(3, TimeUnit.SECONDS).untilAsserted {
             wireMockService.verifyNotifySendLetterCalled()
-            val actualEntity = notificationRepository.getBySourceReferenceAndGssCode(sourceReference, expectedSourceType, listOf(gssCode))
+            val actualEntity = notificationRepository.getBySourceReferenceAndGssCode(
+                sourceReference,
+                sourceTypeEntity,
+                listOf(gssCode)
+            )
             assertThat(actualEntity).hasSize(1).element(0)
                 .extracting("sourceType", "type", "channel")
-                .containsExactlyInAnyOrder(expectedSourceType, NotificationType.REJECTED_DOCUMENT, Channel.LETTER)
+                .containsExactlyInAnyOrder(sourceTypeEntity, notificationType, Channel.LETTER)
             stopWatch.stop()
             logger.info("completed assertions in $stopWatch for language EN")
         }
