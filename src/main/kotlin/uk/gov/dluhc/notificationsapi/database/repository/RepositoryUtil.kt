307bebf3a9c60c4b0789d58819d06f28abe5b93d
Harry Best <150065627+harrybest-softwire@users.noreply.github.com>
2024-09-10 12:46:30 +0100

    EIP1-10517: Move get communication endpoint (#209)
    * EIP1-10517: Adjust get communication endpoint location
    
    * EIP1-10517: Add application id validation test
    
    * EIP1-10517: Fix linter

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/database/repository/RepositoryUtil.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/database/repository/RepositoryUtil.kt
@@ -20,6 +20,10 @@ fun queryRequestWithoutGssCodes(sourceReference: String, sourceType: SourceType)
         .queryConditional(QueryConditional.keyEqualTo(key(sourceReference)))
         .filterExpression(sourceTypeFilterExpression(sourceType))
 
+fun queryRequestWithNotificationId(notificationId: String, sourceReference: String, sourceType: SourceType, gssCodes: List<String>): QueryEnhancedRequest.Builder =
+    queryRequest(sourceReference, sourceType, gssCodes)
+        .filterExpression(notificationIdFilterExpression(notificationId))
+
 fun sourceTypeFilterExpression(sourceType: SourceType): Expression =
     Expression.builder()
         .expression("#sourceType = :sourceType")
@@ -36,6 +40,13 @@ fun gssCodesFilterExpression(gssCodes: List<String>): Expression =
                 filterExpression.putExpressionValue(":gssCode_$index", AttributeValue.fromS(gssCode))
             }
         }.build()
+
+fun notificationIdFilterExpression(notificationId: String): Expression =
+    Expression.builder()
+        .expression("#id = :notificationId")
+        .putExpressionName("#id", "id")
+        .putExpressionValue(":notificationId", AttributeValue.fromS(notificationId))
+        .build()
 
 fun key(partitionValue: String): Key =
     Key.builder().partitionValue(partitionValue).build()
