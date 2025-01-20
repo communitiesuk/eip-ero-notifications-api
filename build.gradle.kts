298fe3b18f464d503db4470249a245defdb98897
Alex Yip <alex.yip@softwire.com>
2025-01-20 11:29:53 +0000

    EROPSPT-403: Bump dependencycheck to 12

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.3.1"
     id("org.openapi.generator") version "7.0.1"
-    id("org.owasp.dependencycheck") version "8.2.1"
+    id("org.owasp.dependencycheck") version "12.0.1"
 }
 
 group = "uk.gov.dluhc"
