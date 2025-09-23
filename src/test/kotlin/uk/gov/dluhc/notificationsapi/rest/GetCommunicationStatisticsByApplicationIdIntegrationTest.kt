385cee17b1938285ca7040eae6625a4480c37180
ClaHur <clare.hurst@softwire.com>
2025-09-23 10:54:01 +0100

    EIP1-13110 Review markup

--- src/test/kotlin/uk/gov/dluhc/notificationsapi/rest/GetCommunicationStatisticsByApplicationIdIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/notificationsapi/rest/GetCommunicationStatisticsByApplicationIdIntegrationTest.kt
@@ -1,7 +1,6 @@
 package uk.gov.dluhc.notificationsapi.rest
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import uk.gov.dluhc.notificationsapi.config.IntegrationTest
@@ -13,45 +12,42 @@ import uk.gov.dluhc.notificationsapi.testsupport.testdata.database.entity.aNotif
 import java.util.UUID
 
 internal class GetCommunicationStatisticsByApplicationIdIntegrationTest : IntegrationTest() {
-    @Nested
-    inner class SharedEndpoint {
-        @ParameterizedTest
-        @CsvSource(
-            value = ["POSTAL,postal", "PROXY,proxy", "VOTER_CARD,voter-card", "OVERSEAS,overseas"],
+    @ParameterizedTest
+    @CsvSource(
+        value = ["POSTAL,postal", "PROXY,proxy", "VOTER_CARD,voter-card", "OVERSEAS,overseas"],
+    )
+    fun `should return correctly built response`(
+        sourceType: SourceType,
+        applicationType: String,
+    ) {
+        // Given
+        val applicationId = aRandomSourceReference()
+
+        notificationRepository.saveNotification(
+            aNotificationBuilder(
+                sourceReference = applicationId,
+                sourceType = sourceType,
+                type = NotificationType.BESPOKE_COMM,
+            ),
         )
-        fun `should return correctly built response`(
-            sourceType: SourceType,
-            applicationType: String,
-        ) {
-            // Given
-            val applicationId = aRandomSourceReference()
 
-            notificationRepository.saveNotification(
-                aNotificationBuilder(
-                    sourceReference = applicationId,
-                    sourceType = sourceType,
-                    type = NotificationType.BESPOKE_COMM,
-                ),
-            )
+        val expected = CommunicationsStatisticsResponse(
+            numBespokeCommunicationsSent = 1,
+            numNotRegisteredToVoteCommsSent = 0,
+            numSignatureRequestCommsSent = 0,
+            numPhotoRequestCommsSent = 0,
+            numIdentityDocumentRequestCommsSent = 0,
+        )
 
-            val expected = CommunicationsStatisticsResponse(
-                numBespokeCommunicationsSent = 1,
-                numNotRegisteredToVoteCommsSent = 0,
-                numSignatureRequestCommsSent = 0,
-                numPhotoRequestCommsSent = 0,
-                numIdentityDocumentRequestCommsSent = 0,
-            )
+        // When
+        val response = webTestClient.get()
+            .uri(buildUri(applicationId = applicationId, service = applicationType))
+            .exchange()
 
-            // When
-            val response = webTestClient.get()
-                .uri(buildUri(applicationId = applicationId, service = applicationType))
-                .exchange()
-
-            // Then
-            response.expectStatus().isOk
-            val actual = response.returnResult(CommunicationsStatisticsResponse::class.java).responseBody.blockFirst()
-            assertThat(actual).isEqualTo(expected)
-        }
+        // Then
+        response.expectStatus().isOk
+        val actual = response.returnResult(CommunicationsStatisticsResponse::class.java).responseBody.blockFirst()
+        assertThat(actual).isEqualTo(expected)
     }
 
     private fun buildUri(applicationId: String = UUID.randomUUID().toString(), service: String) =
