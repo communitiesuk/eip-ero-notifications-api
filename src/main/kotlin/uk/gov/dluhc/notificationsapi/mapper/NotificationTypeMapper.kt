817e2b04f6a22d6ae62a7079de842de61b003bc8
ConWhiSW <connor.whitworth@softwire.com>
2025-10-17 12:15:34 +0100

    EIP1-13129: Add communication to notification type mapper and refactor

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/mapper/NotificationTypeMapper.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/mapper/NotificationTypeMapper.kt
@@ -1,88 +1,110 @@
 package uk.gov.dluhc.notificationsapi.mapper
 
-import org.mapstruct.Mapper
-import org.mapstruct.MappingConstants
-import org.mapstruct.ValueMapping
+import org.springframework.stereotype.Component
 import uk.gov.dluhc.notificationsapi.dto.NotificationType
 import uk.gov.dluhc.notificationsapi.messaging.models.MessageType
 import uk.gov.dluhc.notificationsapi.models.TemplateType
 import uk.gov.dluhc.notificationsapi.database.entity.NotificationType as NotificationTypeEntity
 
-@Mapper
-interface NotificationTypeMapper {
+@Component
+class NotificationTypeMapper {
 
-    @ValueMapping(source = "APPLICATION_MINUS_RECEIVED", target = "APPLICATION_RECEIVED")
-    @ValueMapping(source = "APPLICATION_MINUS_APPROVED", target = "APPLICATION_APPROVED")
-    @ValueMapping(source = "APPLICATION_MINUS_REJECTED", target = "APPLICATION_REJECTED")
-    @ValueMapping(source = "PHOTO_MINUS_RESUBMISSION", target = "PHOTO_RESUBMISSION")
-    @ValueMapping(source = "ID_MINUS_DOCUMENT_MINUS_RESUBMISSION", target = "ID_DOCUMENT_RESUBMISSION")
-    @ValueMapping(source = "ID_MINUS_DOCUMENT_MINUS_REQUIRED", target = "ID_DOCUMENT_REQUIRED")
-    @ValueMapping(source = "REJECTED_MINUS_SIGNATURE", target = "REJECTED_SIGNATURE")
-    @ValueMapping(source = "REQUESTED_MINUS_SIGNATURE", target = "REQUESTED_SIGNATURE")
-    @ValueMapping(source = "REJECTED_MINUS_DOCUMENT", target = "REJECTED_DOCUMENT")
-    @ValueMapping(source = "NINO_MINUS_NOT_MINUS_MATCHED", target = "NINO_NOT_MATCHED")
-    @ValueMapping(source = "PARENT_MINUS_GUARDIAN_MINUS_PROOF_MINUS_REQUIRED", target = "PARENT_GUARDIAN_PROOF_REQUIRED")
-    @ValueMapping(source = "PREVIOUS_MINUS_ADDRESS_MINUS_DOCUMENT_MINUS_REQUIRED", target = "PREVIOUS_ADDRESS_DOCUMENT_REQUIRED")
-    @ValueMapping(source = "BESPOKE_MINUS_COMM", target = "BESPOKE_COMM")
-    @ValueMapping(source = "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE", target = "NOT_REGISTERED_TO_VOTE")
-    @ValueMapping(source = "SIGNATURE_MINUS_RESUBMISSION", target = "SIGNATURE_RESUBMISSION")
-    fun mapMessageTypeToNotificationType(messageType: MessageType): NotificationType
+    fun mapMessageTypeToNotificationType(messageType: MessageType): NotificationType =
+        when (messageType) {
+            MessageType.APPLICATION_MINUS_RECEIVED -> NotificationType.APPLICATION_RECEIVED
+            MessageType.APPLICATION_MINUS_APPROVED -> NotificationType.APPLICATION_APPROVED
+            MessageType.APPLICATION_MINUS_REJECTED -> NotificationType.APPLICATION_REJECTED
+            MessageType.PHOTO_MINUS_RESUBMISSION -> NotificationType.PHOTO_RESUBMISSION
+            MessageType.ID_MINUS_DOCUMENT_MINUS_RESUBMISSION -> NotificationType.ID_DOCUMENT_RESUBMISSION
+            MessageType.ID_MINUS_DOCUMENT_MINUS_REQUIRED -> NotificationType.ID_DOCUMENT_REQUIRED
+            MessageType.REJECTED_MINUS_SIGNATURE -> NotificationType.REJECTED_SIGNATURE
+            MessageType.REQUESTED_MINUS_SIGNATURE -> NotificationType.REQUESTED_SIGNATURE
+            MessageType.REJECTED_MINUS_DOCUMENT -> NotificationType.REJECTED_DOCUMENT
+            MessageType.NINO_MINUS_NOT_MINUS_MATCHED -> NotificationType.NINO_NOT_MATCHED
+            MessageType.PARENT_MINUS_GUARDIAN_MINUS_PROOF_MINUS_REQUIRED -> NotificationType.PARENT_GUARDIAN_PROOF_REQUIRED
+            MessageType.PREVIOUS_MINUS_ADDRESS_MINUS_DOCUMENT_MINUS_REQUIRED -> NotificationType.PREVIOUS_ADDRESS_DOCUMENT_REQUIRED
+            MessageType.BESPOKE_MINUS_COMM -> NotificationType.BESPOKE_COMM
+            MessageType.NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE -> NotificationType.NOT_REGISTERED_TO_VOTE
+            MessageType.SIGNATURE_MINUS_RESUBMISSION -> NotificationType.SIGNATURE_RESUBMISSION
+            MessageType.SIGNATURE_MINUS_RECEIVED -> NotificationType.SIGNATURE_RECEIVED
+        }
 
-    // PHOTO_RESUBMISSION_WITH_REASONS is an implementation detail and not a "business" notification type
-    // Therefore it should be saved to the database as PHOTO_RESUBMISSION
-    @ValueMapping(source = "PHOTO_RESUBMISSION_WITH_REASONS", target = "PHOTO_RESUBMISSION")
-    // ID_DOCUMENT_RESUBMISSION_WITH_REASONS is an implementation detail and not a "business" notification type
-    // Therefore it should be saved to the database as ID_DOCUMENT_RESUBMISSION
-    @ValueMapping(source = "ID_DOCUMENT_RESUBMISSION_WITH_REASONS", target = "ID_DOCUMENT_RESUBMISSION")
-    // REJECTED_SIGNATURE_WITH_REASONS is an implementation detail and not a "business" notification type
-    // Therefore it should be saved to the database as REJECTED_SIGNATURE
-    @ValueMapping(source = "REJECTED_SIGNATURE_WITH_REASONS", target = "REJECTED_SIGNATURE")
-    // NINO_NOT_MATCHED_RESTRICTED_DOCUMENTS_LIST is an implementation detail and not a "business" notification type
-    // Therefore it should be saved to the database as NINO_NOT_MATCHED
-    @ValueMapping(source = "NINO_NOT_MATCHED_RESTRICTED_DOCUMENTS_LIST", target = "NINO_NOT_MATCHED")
-    // SIGNATURE_RESUBMISSION_WITH_REASONS is an implementation detail and not a "business" notification type
-    // Therefore it should be saved to the database as SIGNATURE_RESUBMISSION
-    @ValueMapping(source = "SIGNATURE_RESUBMISSION_WITH_REASONS", target = "SIGNATURE_RESUBMISSION")
-    fun toNotificationTypeEntity(notificationType: NotificationType): NotificationTypeEntity
+    // <TEMPLATE_NAME>_WITH_REASONS is an implementation detail and not a "business" notification type
+    // Therefore it should be saved to the database as <TEMPLATE_NAME>
+    // Similarly NINO_NOT_MATCHED_RESTRICTED_DOCUMENTS_LIST
+    fun toNotificationTypeEntity(notificationType: NotificationType): NotificationTypeEntity =
+        when (notificationType) {
+            NotificationType.APPLICATION_RECEIVED -> NotificationTypeEntity.APPLICATION_RECEIVED
+            NotificationType.APPLICATION_APPROVED -> NotificationTypeEntity.APPLICATION_APPROVED
+            NotificationType.APPLICATION_REJECTED -> NotificationTypeEntity.APPLICATION_REJECTED
+            NotificationType.PHOTO_RESUBMISSION,
+            NotificationType.PHOTO_RESUBMISSION_WITH_REASONS,
+            -> NotificationTypeEntity.PHOTO_RESUBMISSION
+            NotificationType.ID_DOCUMENT_RESUBMISSION,
+            NotificationType.ID_DOCUMENT_RESUBMISSION_WITH_REASONS,
+            -> NotificationTypeEntity.ID_DOCUMENT_RESUBMISSION
+            NotificationType.ID_DOCUMENT_REQUIRED -> NotificationTypeEntity.ID_DOCUMENT_REQUIRED
+            NotificationType.REJECTED_DOCUMENT -> NotificationTypeEntity.REJECTED_DOCUMENT
+            NotificationType.REJECTED_SIGNATURE,
+            NotificationType.REJECTED_SIGNATURE_WITH_REASONS,
+            -> NotificationTypeEntity.REJECTED_SIGNATURE
+            NotificationType.REQUESTED_SIGNATURE -> NotificationTypeEntity.REQUESTED_SIGNATURE
+            NotificationType.NINO_NOT_MATCHED,
+            NotificationType.NINO_NOT_MATCHED_RESTRICTED_DOCUMENTS_LIST,
+            -> NotificationTypeEntity.NINO_NOT_MATCHED
+            NotificationType.REJECTED_PARENT_GUARDIAN -> NotificationTypeEntity.REJECTED_PARENT_GUARDIAN
+            NotificationType.REJECTED_PREVIOUS_ADDRESS -> NotificationTypeEntity.REJECTED_PREVIOUS_ADDRESS
+            NotificationType.PARENT_GUARDIAN_PROOF_REQUIRED -> NotificationTypeEntity.PARENT_GUARDIAN_PROOF_REQUIRED
+            NotificationType.PREVIOUS_ADDRESS_DOCUMENT_REQUIRED -> NotificationTypeEntity.PREVIOUS_ADDRESS_DOCUMENT_REQUIRED
+            NotificationType.BESPOKE_COMM -> NotificationTypeEntity.BESPOKE_COMM
+            NotificationType.NOT_REGISTERED_TO_VOTE -> NotificationTypeEntity.NOT_REGISTERED_TO_VOTE
+            NotificationType.SIGNATURE_RESUBMISSION,
+            NotificationType.SIGNATURE_RESUBMISSION_WITH_REASONS,
+            -> NotificationTypeEntity.SIGNATURE_RESUBMISSION
+            NotificationType.SIGNATURE_RECEIVED -> NotificationTypeEntity.SIGNATURE_RECEIVED
+        }
 
-    fun toNotificationTypeDto(notificationTypeEntity: NotificationTypeEntity): NotificationType
+    fun toNotificationTypeDto(notificationTypeEntity: NotificationTypeEntity): NotificationType =
+        when (notificationTypeEntity) {
+            NotificationTypeEntity.APPLICATION_RECEIVED -> NotificationType.APPLICATION_RECEIVED
+            NotificationTypeEntity.APPLICATION_APPROVED -> NotificationType.APPLICATION_APPROVED
+            NotificationTypeEntity.APPLICATION_REJECTED -> NotificationType.APPLICATION_REJECTED
+            NotificationTypeEntity.PHOTO_RESUBMISSION -> NotificationType.PHOTO_RESUBMISSION
+            NotificationTypeEntity.ID_DOCUMENT_RESUBMISSION -> NotificationType.ID_DOCUMENT_RESUBMISSION
+            NotificationTypeEntity.ID_DOCUMENT_REQUIRED -> NotificationType.ID_DOCUMENT_REQUIRED
+            NotificationTypeEntity.REJECTED_DOCUMENT -> NotificationType.REJECTED_DOCUMENT
+            NotificationTypeEntity.REJECTED_SIGNATURE -> NotificationType.REJECTED_SIGNATURE
+            NotificationTypeEntity.REQUESTED_SIGNATURE -> NotificationType.REQUESTED_SIGNATURE
+            NotificationTypeEntity.NINO_NOT_MATCHED -> NotificationType.NINO_NOT_MATCHED
+            NotificationTypeEntity.REJECTED_PARENT_GUARDIAN -> NotificationType.REJECTED_PARENT_GUARDIAN
+            NotificationTypeEntity.REJECTED_PREVIOUS_ADDRESS -> NotificationType.REJECTED_PREVIOUS_ADDRESS
+            NotificationTypeEntity.PARENT_GUARDIAN_PROOF_REQUIRED -> NotificationType.PARENT_GUARDIAN_PROOF_REQUIRED
+            NotificationTypeEntity.PREVIOUS_ADDRESS_DOCUMENT_REQUIRED -> NotificationType.PREVIOUS_ADDRESS_DOCUMENT_REQUIRED
+            NotificationTypeEntity.BESPOKE_COMM -> NotificationType.BESPOKE_COMM
+            NotificationTypeEntity.NOT_REGISTERED_TO_VOTE -> NotificationType.NOT_REGISTERED_TO_VOTE
+            NotificationTypeEntity.SIGNATURE_RESUBMISSION -> NotificationType.SIGNATURE_RESUBMISSION
+            NotificationTypeEntity.SIGNATURE_RECEIVED -> NotificationType.SIGNATURE_RECEIVED
+        }
 
-    @ValueMapping(source = "APPLICATION_RECEIVED", target = "APPLICATION_MINUS_RECEIVED")
-    @ValueMapping(source = "APPLICATION_APPROVED", target = "APPLICATION_MINUS_APPROVED")
-    @ValueMapping(source = "APPLICATION_REJECTED", target = "APPLICATION_MINUS_REJECTED")
-    @ValueMapping(source = "PHOTO_RESUBMISSION", target = "PHOTO_MINUS_RESUBMISSION")
-    @ValueMapping(source = "ID_DOCUMENT_RESUBMISSION", target = "ID_MINUS_DOCUMENT_MINUS_RESUBMISSION")
-    @ValueMapping(source = "ID_DOCUMENT_REQUIRED", target = "ID_MINUS_DOCUMENT_MINUS_REQUIRED")
-    @ValueMapping(source = "REJECTED_DOCUMENT", target = "REJECTED_MINUS_DOCUMENT")
-    @ValueMapping(source = "REJECTED_SIGNATURE", target = "REJECTED_MINUS_SIGNATURE")
-    @ValueMapping(source = "REQUESTED_SIGNATURE", target = "REQUESTED_MINUS_SIGNATURE")
-    @ValueMapping(source = "NINO_NOT_MATCHED", target = "NINO_MINUS_NOT_MINUS_MATCHED")
-    @ValueMapping(source = "REJECTED_PARENT_GUARDIAN", target = "REJECTED_MINUS_PARENT_MINUS_GUARDIAN")
-    @ValueMapping(source = "REJECTED_PREVIOUS_ADDRESS", target = "REJECTED_MINUS_PREVIOUS_MINUS_ADDRESS")
-    @ValueMapping(
-        source = "PARENT_GUARDIAN_PROOF_REQUIRED",
-        target = "PARENT_MINUS_GUARDIAN_MINUS_PROOF_MINUS_REQUIRED",
-    )
-    @ValueMapping(
-        source = "PREVIOUS_ADDRESS_DOCUMENT_REQUIRED",
-        target = "PREVIOUS_MINUS_ADDRESS_MINUS_DOCUMENT_MINUS_REQUIRED",
-    )
-    @ValueMapping(source = "BESPOKE_COMM", target = "BESPOKE_MINUS_COMM")
-    @ValueMapping(source = "NOT_REGISTERED_TO_VOTE", target = "NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE")
-    @ValueMapping(source = "SIGNATURE_RESUBMISSION", target = "SIGNATURE_MINUS_RESUBMISSION")
-    // Mappings
-    // - NotificationType.PHOTO_RESUBMISSION_WITH_REASONS
-    // - NotificationType.ID_DOCUMENT_RESUBMISSION_WITH_REASONS
-    // - NotificationType.REJECTED_SIGNATURE_WITH_REASONS
-    // - NotificationType.NINO_NOT_MATCHED_RESTRICTED_DOCUMENTS_LIST
-    // - NotificationType.SIGNATURE_RESUBMISSION_WITH_REASONS
-    // to the REST API (TemplateType) are not supported and will never happen because they are not saved as database
-    // enums, so they will never be presented in this method call. MapStruct does not know this though, so makes us
-    // handle the scenario
-    @ValueMapping(source = "PHOTO_RESUBMISSION_WITH_REASONS", target = MappingConstants.THROW_EXCEPTION)
-    @ValueMapping(source = "ID_DOCUMENT_RESUBMISSION_WITH_REASONS", target = MappingConstants.THROW_EXCEPTION)
-    @ValueMapping(source = "REJECTED_SIGNATURE_WITH_REASONS", target = MappingConstants.THROW_EXCEPTION)
-    @ValueMapping(source = "NINO_NOT_MATCHED_RESTRICTED_DOCUMENTS_LIST", target = MappingConstants.THROW_EXCEPTION)
-    @ValueMapping(source = "SIGNATURE_RESUBMISSION_WITH_REASONS", target = MappingConstants.THROW_EXCEPTION)
-    fun fromNotificationTypeDtoToTemplateTypeApi(notificationType: NotificationType): TemplateType
+    fun fromNotificationTypeDtoToTemplateTypeApi(notificationType: NotificationType): TemplateType =
+        when (notificationType) {
+            NotificationType.APPLICATION_RECEIVED -> TemplateType.APPLICATION_MINUS_RECEIVED
+            NotificationType.APPLICATION_APPROVED -> TemplateType.APPLICATION_MINUS_APPROVED
+            NotificationType.APPLICATION_REJECTED -> TemplateType.APPLICATION_MINUS_REJECTED
+            NotificationType.PHOTO_RESUBMISSION -> TemplateType.PHOTO_MINUS_RESUBMISSION
+            NotificationType.ID_DOCUMENT_RESUBMISSION -> TemplateType.ID_MINUS_DOCUMENT_MINUS_RESUBMISSION
+            NotificationType.ID_DOCUMENT_REQUIRED -> TemplateType.ID_MINUS_DOCUMENT_MINUS_REQUIRED
+            NotificationType.REJECTED_DOCUMENT -> TemplateType.REJECTED_MINUS_DOCUMENT
+            NotificationType.REJECTED_SIGNATURE -> TemplateType.REJECTED_MINUS_SIGNATURE
+            NotificationType.REQUESTED_SIGNATURE -> TemplateType.REQUESTED_MINUS_SIGNATURE
+            NotificationType.NINO_NOT_MATCHED -> TemplateType.NINO_MINUS_NOT_MINUS_MATCHED
+            NotificationType.REJECTED_PARENT_GUARDIAN -> TemplateType.REJECTED_MINUS_PARENT_MINUS_GUARDIAN
+            NotificationType.REJECTED_PREVIOUS_ADDRESS -> TemplateType.REJECTED_MINUS_PREVIOUS_MINUS_ADDRESS
+            NotificationType.PARENT_GUARDIAN_PROOF_REQUIRED -> TemplateType.PARENT_MINUS_GUARDIAN_MINUS_PROOF_MINUS_REQUIRED
+            NotificationType.PREVIOUS_ADDRESS_DOCUMENT_REQUIRED -> TemplateType.PREVIOUS_MINUS_ADDRESS_MINUS_DOCUMENT_MINUS_REQUIRED
+            NotificationType.BESPOKE_COMM -> TemplateType.BESPOKE_MINUS_COMM
+            NotificationType.NOT_REGISTERED_TO_VOTE -> TemplateType.NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE
+            NotificationType.SIGNATURE_RESUBMISSION -> TemplateType.SIGNATURE_MINUS_RESUBMISSION
+            else -> throw IllegalArgumentException("Unexpected NotificationType $notificationType when mapping to API TemplateType")
+        }
 }
