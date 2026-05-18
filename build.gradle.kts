351380de9a8486f236aed7e93de33c1f6ae14727
Mina Lui <mina.lui@softwire.com>
2026-05-18 12:00:56 +0100

    EROPSPT-710: Fix tomcat version (#297)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,12 @@ ext["snakeyaml.version"] = "2.2"
 extra["springCloudAwsVersion"] = "3.1.1"
 extra["awsSdkVersion"] = "2.26.20"
 
-// Forcing 4.1.133 version of netty to patch vulnerabilities, under EROPSPT-710.
-// When we upgrade to spring v4 we should check if spring pulls in newer versions of netty.
+// Forcing 4.1.133 version of netty and 10.1.55 version of tomcat to patch vulnerabilities, under EROPSPT-710.
+// When we upgrade to spring v4 we should check if spring pulls in newer versions of netty and tomcat.
 // If so, this override should be removed.
 // TODO EROPSPT-603
 extra["netty.version"] = "4.1.133.Final"
+extra["tomcat.version"] = "10.1.55"
 
 allOpen {
     annotations("jakarta.persistence.Entity", "jakarta.persistence.MappedSuperclass", "jakarta.persistence.Embedabble")
