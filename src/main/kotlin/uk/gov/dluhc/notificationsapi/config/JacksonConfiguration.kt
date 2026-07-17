e9ddd7aa52f1d9b6f7c0769370721e5123d16b6f
ConWhiSW <connor.whitworth@softwire.com>
2026-07-17 12:14:38 +0100

    EROPSPT-603: Disable FAIL_ON_NULL_FOR_PRIMITIVES

--- src/main/kotlin/uk/gov/dluhc/notificationsapi/config/JacksonConfiguration.kt
+++ src/main/kotlin/uk/gov/dluhc/notificationsapi/config/JacksonConfiguration.kt
@@ -18,6 +18,7 @@ class JacksonConfiguration {
             .addModule(KotlinModule.Builder().build())
             .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
             .disable(DateTimeFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS)
+            .disable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
             .changeDefaultPropertyInclusion { _ -> JsonInclude.Value.construct(Include.NON_NULL, Include.NON_NULL) }
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .build()
