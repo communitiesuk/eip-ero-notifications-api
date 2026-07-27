9f8c8d2c3fb955ab78008c4bc665d63aaa11931a
Mina Lui SW <mina.lui@softwire.com>
2026-07-27 14:14:30 +0100

    EROPSPT-733: Remove redundant compiler arg

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,7 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+        freeCompilerArgs.addAll("-Xjsr305=strict")
         jvmTarget.set(JvmTarget.JVM_17)
     }
 }
